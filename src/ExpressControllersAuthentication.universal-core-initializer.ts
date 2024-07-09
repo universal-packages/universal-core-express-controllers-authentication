cd35c7e0c430510f522f6f63495458ccf81380fe
David De Anda <omarandstuff@gmail.com>
2024-07-08 19:21:51 -0600

    Naming consistency finishing

--- src/ExpressControllersAuthentication.universal-core-initializer.ts
+++ src/ExpressControllersAuthentication.universal-core-initializer.ts
@@ -2,6 +2,7 @@ import CoreInitializer from '@universal-packages/core/CoreInitializer'
 
 export default class ExpressControllersAuthenticationInitializer extends CoreInitializer {
   public static readonly initializerName = 'express-controllers-authentication'
+  public static readonly description: string = 'Core Express Controllers Authentication Initializer'
 
   public readonly templatesLocation: string = `${__dirname}/templates`
 }
