dfbfda1d7cb74e72598a914995acdd3ba6861cbf
David De Anda <omarandstuff@gmail.com>
2024-07-11 15:05:12 -0600

    Right initializer import

--- src/ExpressControllersAuthentication.universal-core-initializer.ts
+++ src/ExpressControllersAuthentication.universal-core-initializer.ts
@@ -1,4 +1,4 @@
-import CoreInitializer from '@universal-packages/core/CoreInitializer'
+import { CoreInitializer } from '@universal-packages/core'
 
 export default class ExpressControllersAuthenticationInitializer extends CoreInitializer {
   public static readonly initializerName = 'express-controllers-authentication'
